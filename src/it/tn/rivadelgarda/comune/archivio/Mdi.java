2bfdb468e3a82a97caa283bf5d28fda062526fcc
Michela Piva <pivamichela@comune.rivadelgarda.tn.it>
2014-06-30 17:38:13 +0200

    Struttura applicazione e prima finestra di test del Protocollo

---
+++ src/it/tn/rivadelgarda/comune/archivio/Mdi.java
@@ -0,0 +1,302 @@
+/*
+ * Copyright (C) 2012 AXIA Studio (http://www.axiastudio.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.tn.rivadelgarda.comune.archivio;
+
+import com.axiastudio.pypapi.Register;
+import com.axiastudio.pypapi.db.Database;
+import com.axiastudio.pypapi.db.IDatabase;
+import com.axiastudio.pypapi.db.IFactory;
+import com.axiastudio.pypapi.db.Store;
+import com.axiastudio.pypapi.ui.IForm;
+import com.axiastudio.pypapi.ui.IUIFile;
+import com.axiastudio.pypapi.ui.Util;
+import com.axiastudio.pypapi.ui.Window;
+import com.trolltech.qt.core.QObject;
+import com.trolltech.qt.core.QSignalMapper;
+import com.trolltech.qt.gui.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Tiziano Lattisi <tiziano at axiastudio.it>
+ */
+public class Mdi extends QMainWindow implements IMdi {
+    
+    private static String ICON = "classpath:com/axiastudio/pypapi/ui/resources/pypapi32.png";
+    private QMdiArea workspace;
+    private QTreeWidget tree;
+    private QSystemTrayIcon trayIcon;
+    private QMenu menuWindows;
+    private QAction actionCloseAll;
+    private QAction actionTile;
+    private QAction actionCascade;
+    private QAction actionNext;
+    private QAction actionPrevious;
+    private QAction actionSeparator;
+    private QAction actionClose;
+    private QSignalMapper windowMapper;
+    
+    public Mdi(){
+        this.setWindowIcon(new QIcon(ICON));
+        this.createWorkspace();
+        this.createTree();
+        //this.createSystemTray();
+        this.createMenu();
+
+        Register.registerUtility(this, IMdi.class);
+
+    }
+    
+    private void createMenu(){
+        menuWindows = this.menuBar().addMenu("Finestre");
+        actionClose = new QAction("Chiudi", this);
+        actionClose.triggered.connect(this.workspace, "closeActiveSubWindow()");
+        actionCloseAll = new QAction("Chiudi tutte", this);
+        actionCloseAll.triggered.connect(this.workspace, "closeAllSubWindows()");
+        actionTile = new QAction("Allinea", this);
+        actionTile.triggered.connect(this.workspace, "tileSubWindows()");
+        actionCascade = new QAction("Disponi a cascata", this);
+        actionCascade.triggered.connect(this.workspace, "cascadeSubWindows()");
+        actionNext = new QAction("Finestra successiva", this);
+        actionNext.triggered.connect(this.workspace, "activateNextSubWindow()");
+        actionPrevious = new QAction("Finestra precedente", this);
+        actionPrevious.triggered.connect(this.workspace, "activatePreviousSubWindow()");
+        actionSeparator = new QAction(this);
+        actionSeparator.setSeparator(true);
+        
+        menuWindows.aboutToShow.connect(this, "refreshMenuWindows()");     
+    }
+    
+    private void refreshMenuWindows(){
+        
+        menuWindows.clear();
+        menuWindows.addAction(actionClose);
+        menuWindows.addAction(actionCloseAll);
+        menuWindows.addAction(actionSeparator);
+        menuWindows.addAction(actionTile);
+        menuWindows.addAction(actionCascade);
+        menuWindows.addAction(actionSeparator);
+        menuWindows.addAction(actionNext);
+        menuWindows.addAction(actionPrevious);
+        menuWindows.addAction(actionSeparator);
+        menuWindows.addAction(actionCloseAll);
+        
+        for( QMdiSubWindow subWindow: this.workspace.subWindowList() ){
+            
+            String title="";
+            if( subWindow.widget() instanceof QMainWindow ){
+                title = ((QMainWindow) subWindow.widget()).windowTitle();
+            }
+            if( subWindow.widget() instanceof QDialog ){
+                title = ((QDialog) subWindow.widget()).windowTitle();
+            }
+            
+            QAction action = menuWindows.addAction(title);
+            action.setCheckable(true);
+            action.setChecked(subWindow.equals(this.workspace.activeSubWindow()));
+            action.triggered.connect(windowMapper, "map()");
+            windowMapper.setMapping(action, subWindow);
+        }
+    }
+    
+    private void createSystemTray(){
+        QMenu menu = new QMenu(this);
+        menu.addAction("prova");
+        this.trayIcon = new QSystemTrayIcon(new QIcon(ICON), this);
+        this.trayIcon.setContextMenu(menu);
+        this.trayIcon.show();
+        this.trayIcon.showMessage("PyPaPi Suite", "Applicazione avviata.");
+        
+    }
+    
+    private void createWorkspace() {
+        QSplitter splitter = new QSplitter();
+        this.tree = new QTreeWidget(splitter);
+        this.workspace = new QMdiArea(splitter);
+        this.setCentralWidget(splitter);        
+        this.workspace.subWindowActivated.connect(this, "refreshMenuWindows()");
+        windowMapper = new QSignalMapper(this);
+        windowMapper.mappedQObject.connect(this, "setActiveSubWindow(QObject)");
+    }
+    
+    private void setActiveSubWindow(QObject obj){
+        this.workspace.setActiveSubWindow((QMdiSubWindow) obj);
+    }
+    
+    private void createTree() {
+        this.tree.setColumnCount(2);
+        this.tree.setHeaderLabel("Gestione archivio");
+        this.tree.setColumnHidden(1, true);
+
+
+        /* scrivania */
+        QTreeWidgetItem itemScrivania = new QTreeWidgetItem(this.tree);
+        itemScrivania.setText(0, "Scrivania");
+        itemScrivania.setIcon(0, new QIcon("classpath:it/tn/rivadelgarda/comune/archivio/resources/house.png"));
+        itemScrivania.setText(1, "SCRIVANIA");
+
+        QTreeWidgetItem itemProtocollo = new QTreeWidgetItem(this.tree);
+        itemProtocollo.setText(0, "Protocollo");
+        itemProtocollo.setText(1, "it.tn.rivadelgarda.comune.archivio.entities.Protocollo");
+        itemProtocollo.setText(2, "NEW");
+
+        this.tree.activated.connect(this, "runTask()");
+        this.tree.setMinimumWidth(200);
+
+    }
+    
+    private void runTask() {
+        String formName = this.tree.currentItem().text(1);
+        if (formName == null || formName.equals("")) {
+            return;  // item di raggruppamento
+        }
+
+        String mode = this.tree.currentItem().text(2);
+        /* chianata form speciale */
+        if( "XXXXXX".equals(formName) ){
+/*            Xxxxx form = new Xxxx();
+            this.workspace.addSubWindow(form);
+            form.show();  */
+/*           Xxxx passDlg = new Xxxxx(this);
+            this.workspace.addSubWindow(xxxxxx);
+            int exec = Xxxx.exec();   */
+        } else {
+            /* form registrata */
+            Window form=null;
+            Class<? extends Window> formClass = (Class) Register.queryUtility(IForm.class, formName);
+            String uiFile = (String) Register.queryUtility(IUIFile.class, formName);
+            Class factory = (Class) Register.queryUtility(IFactory.class, formName);
+            try {
+                Constructor<? extends Window> constructor = formClass.getConstructor(new Class[]{String.class, Class.class, String.class});
+                try {
+                    form = constructor.newInstance(new Object[]{uiFile, factory, ""});
+                } catch (InstantiationException ex) {
+                    Logger.getLogger(Mdi.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(Mdi.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalArgumentException ex) {
+                    Logger.getLogger(Mdi.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InvocationTargetException ex) {
+                    Logger.getLogger(Mdi.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } catch (NoSuchMethodException ex) {
+                Logger.getLogger(Mdi.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SecurityException ex) {
+                Logger.getLogger(Mdi.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            // A store with a new element
+            Store store = null;
+
+            if( "NEW".equals(mode) ){
+                store = new Store(new ArrayList<Object>());
+                try {
+                    Constructor entityConstructor = factory.getConstructor(new Class[]{});
+                    Object entity = entityConstructor.newInstance(new Object[]{});
+                    store.add(entity);
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            } else if( mode.startsWith("NAMEDQUERY") ){
+                String[] split = mode.split(":");
+                String namedQueryName = split[1];
+
+                Database db = (Database) Register.queryUtility(IDatabase.class);
+                EntityManagerFactory emf = db.getEntityManagerFactory();
+                EntityManager em = emf.createEntityManager();
+                TypedQuery namedQuery = em.createNamedQuery(namedQueryName, factory);
+
+                // TODO: aggiungere qualche controllo?
+                for( Integer i=2; i<split.length; i++){
+                    String parameters = split[i];
+                    String[] split1 = parameters.split(",");
+                    String fieldName = split1[0];
+                    String typeName = split1[1];
+                    String stringValue = split1[2];
+                    if( "Integer".equals(typeName) ){
+                        Object value = Integer.parseInt(stringValue);
+                        namedQuery = namedQuery.setParameter(fieldName, value);
+                    }
+                }
+
+                List<?> resultList = namedQuery.getResultList();
+                store = new Store(resultList);
+            }
+
+            if( store != null ){
+                if ( store.size()>0 ) {
+                    form.init(store);
+                }
+                else {
+                    Util.warningBox(this, "Attenzione", "Nessun record trovato");
+                    return;
+                }
+            } else {
+                form.init();
+            }
+            this.workspace.addSubWindow(form);
+            this.showForm(form);
+            if( store != null ) {
+                form.getContext().getDirty();
+            }
+            this.menuWindows.addAction(form.toString());
+        }
+    }
+    
+    private void showForm(Window form) {
+        if( this.workspace.subWindowList().size()>1 ){
+            form.show();
+        } else {
+            form.showMaximized();
+        }
+    }
+
+    @Override
+    public QMdiArea getWorkspace() {
+        return workspace;
+    }
+
+    @Override
+    protected void closeEvent(QCloseEvent event) {
+        boolean iHaveToClose = true;
+        for( QMdiSubWindow subWindow: this.workspace.subWindowList() ){
+            if( subWindow.widget() instanceof QMainWindow ){
+                subWindow.widget().setFocus();
+                iHaveToClose = iHaveToClose && subWindow.close();
+            }
+        }
+        if( iHaveToClose ) {
+            super.closeEvent(event);
+            return;
+        }
+        event.ignore();
+    }
+}
---
+++ src/it/tn/rivadelgarda/comune/archivio/Start.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2013 Comune di Riva del Garda
+ * (http://www.comune.rivadelgarda.tn.it)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.tn.rivadelgarda.comune.archivio;
+
+import com.axiastudio.pypapi.Application;
+import com.axiastudio.pypapi.Register;
+import com.axiastudio.pypapi.db.Database;
+import com.axiastudio.pypapi.db.IDatabase;
+import com.axiastudio.pypapi.ui.Window;
+import it.tn.rivadelgarda.comune.archivio.entities.Protocollo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Tiziano Lattisi <tiziano at axiastudio.it>
+ * 
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Application app = new Application(args);
+        InputStream propertiesStream = Start.class.getResourceAsStream("gda.properties");
+
+        app.setLanguage("it");
+
+        Database db = (Database) Register.queryUtility(IDatabase.class);
+        configure(app, propertiesStream);
+
+
+        Mdi mdi = new Mdi();
+        mdi.showMaximized();
+        mdi.setWindowTitle("Archivio");
+        mdi.show();
+
+        app.setCustomApplicationName("Archivio");
+        app.exec();
+
+        System.exit(0);
+    }
+
+    protected static void configure(Application app, InputStream propertiesStream) {
+
+        String jdbcUrl = null;
+        String jdbcUser = null;
+        String jdbcPassword = null;
+        String jdbcDriver = null;
+
+        String logLevel = null;
+
+        // file di Properties
+        Properties properties = new Properties();
+        try {
+            if( propertiesStream != null ) {
+                properties.load(propertiesStream);
+
+                jdbcUrl = properties.getProperty("jdbc.url");
+                jdbcUser = properties.getProperty("jdbc.user");
+                jdbcPassword = properties.getProperty("jdbc.password");
+                jdbcDriver = properties.getProperty("jdbc.driver");
+
+                logLevel = properties.getProperty("suite.loglevel");
+            }
+        } catch (IOException e) {
+            String message = "Unable to read properties file: " + propertiesStream;
+            Logger.getLogger(Start.class.getName()).log(Level.WARNING, message, e);
+        }
+
+        /*
+         *  OVerriding della configurazione
+         */
+
+        // jdbc
+        if( System.getProperty("jdbc.url") != null ) {
+            jdbcUrl = System.getProperty("jdbc.url");
+        }
+        if( System.getProperty("jdbc.user") != null ) {
+            jdbcUser = System.getProperty("jdbc.user");
+        }
+        if( System.getProperty("jdbc.password") != null ) {
+            jdbcPassword = System.getProperty("jdbc.password");
+        }
+        if( System.getProperty("jdbc.driver") != null ) {
+            jdbcDriver = System.getProperty("jdbc.driver");
+        }
+        // log
+        if( System.getProperty("suite.loglevel") != null ) {
+            logLevel = System.getProperty("suite.loglevel");
+        }
+
+        Map mapProperties = new HashMap();
+        mapProperties.put("javax.persistence.jdbc.url", jdbcUrl);
+        if( jdbcUser != null ){
+            mapProperties.put("javax.persistence.jdbc.user", jdbcUser);
+        }
+        if( jdbcPassword != null ){
+            mapProperties.put("javax.persistence.jdbc.password", jdbcPassword);
+        }
+        if( jdbcDriver != null ){
+            mapProperties.put("javax.persistence.jdbc.driver", jdbcDriver);
+        }
+        if( logLevel != null ){
+            mapProperties.put("eclipselink.logging.level", logLevel);
+            mapProperties.put("eclipselink.logging.parameters", "true");
+        }
+
+        Database db = new Database();
+        mapProperties.put("eclipselink.ddl-generation", "");
+        db.open("SuitePU", mapProperties);
+        Register.registerUtility(db, IDatabase.class);
+
+        // jdbc
+        app.setConfigItem("jdbc.url", jdbcUrl);
+
+        Register.registerForm(db.getEntityManagerFactory(),
+                "classpath:it/tn/rivadelgarda/comune/archivio/forms/Protocollo.ui",
+                Protocollo.class,
+                Window.class
+        );
+
+
+    }
+
+}
